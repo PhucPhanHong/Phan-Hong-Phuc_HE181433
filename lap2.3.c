9cb16c3469a8a40787a2630fdd3dd91b1cb6486a
PhucPhanHong <156642685+PhucPhanHong@users.noreply.github.com>
2024-01-19 15:27:56 +0700

    lap2.3.c

--- lap2.3.c
+++ lap2.3.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 int main() {
     int num, sum = 0;
     printf("Nhap cac so nguyen (nhap 0 de dung):\n");
-   Do{ printf("Nhap mot so nguyen: ");
+    while (num != 0) {
+        printf("Nhap mot so nguyen: ");
         scanf("%d", &num);
         sum += num;
         printf("Tong hien tai: %d\n", sum);
@@ -10,7 +12,7 @@ int main() {
             printf(" Dung lai.\n");
             break;
         }
+    }
     printf("Tong: %d\n", sum);
     return 0;
 }
-
