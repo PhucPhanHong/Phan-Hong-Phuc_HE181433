3037b599a905c57930c18ba118db7cc0a99240be
PhucPhanHong <156642685+PhucPhanHong@users.noreply.github.com>
2024-01-19 15:47:04 +0700

    lap2.2.c

--- lap2.2.c
+++ lap2.2.c
@@ -1,38 +1,47 @@
 #include <stdio.h>
-#include <math.h>
-int main(){
-	long long tf, n, ti, income;
-	long long pa = 9000000, pd = 3600000;
-
-	printf("Your income of this year:");
-	scanf("%lld", &income);
-	printf("Number of dependent:");
-	scanf("%lld", &n);	
-	
-	tf = 12 * (pa + n * pd);
-	ti = income - tf;
-	
-	printf("tax-free income :%lld\n", tf);
-	if(ti <= 0 ){
-		printf("Taxable income: 0\n");
-		printf("income tax: 0");
-	}
-	else if(ti <= 5000000){
-		printf("Taxable income: %lld\n", ti);
-		printf("Income tax: %lld", ti * 5 / 100);
-	}
-	else if(ti <= 10000000 ){
-		printf("Taxable income: %lld\n", ti);
-		printf("Income tax: %lld", ti * 10 / 100);
-	}
-	else if(ti <= 18000000 ){
-		printf("Taxable income: %lld\n", ti);
-		printf("Income tax: %lld", ti * 15 / 100);
-	}
-	else{
-		printf("Taxable income: %lld\n", ti);
-		printf("Income tax: %lld", ti * 20 / 100);
-	}
-	return 0;
+int main()
+{
+    long pa = 9000000, pd = 3600000;
+    long tf, n, ti, income;
+    
+    printf("Your income this year: ");
+    scanf("%ld", &income);
+    
+    printf("Number of dependents: ");
+    scanf("%ld", &n);
+    
+    tf = 12 * (pa + n * pd);
+    
+    printf("Tax-free income: %ld\n", tf);
+    
+    ti = income - tf;
+    
+    if (ti <= 0)
+    {
+        printf("Taxable income: 0\n");
+        printf("Income tax: 0\n");
+    }
+    else if (ti > 0 && ti <= 5000000)
+    {
+        printf("Taxable income: %ld\n", ti);
+        printf("Income tax: %ld\n", (long)(ti * 0.05));
+    }
+    else if (ti >= 5000001 && ti <= 10000000)
+    {
+        printf("Taxable income: %ld\n", ti);
+        printf("Income tax: %ld\n", (long)(5000000 * 0.05 + (ti - 5000000) * 0.1));
+    }
+    else if (ti >= 10000001 && ti <= 18000000)
+    {
+        printf("Taxable income: %ld\n", ti);
+        printf("Income tax: %ld\n", (long)(5000000 * 0.05 + 5000000 * 0.1 + (ti - 10000000) * 0.15));
+    }
+    else
+    {
+        printf("Taxable income: %ld\n", ti);
+        printf("Income tax: %ld\n", (long)(5000000 * 0.05 + 5000000 * 0.1 + 8000000 * 0.15 + (ti - 18000000) * 0.2));
+    }
+    
+    return 0;
 }
 
